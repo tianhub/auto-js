638d7b8938fd9b4eff358e0a02621485a2522950
tian <1149375181@qq.com>
2021-10-25 10:02:13 +0800

    fix: 完善抢券功能

--- vivo领券新/server.js
+++ vivo领券新/server.js
@@ -1,4 +1,4 @@
-let testTime = [16, 59, 59, [820, 999]]; // 时 分 秒 毫秒区间
+let testTime = [9, 59, 59, [900, 999]]; // 时 分 秒 毫秒区间
 
 const refresh = () => { // 刷新页面
   id("game_actionbar_overflow_btn").findOne().click()
@@ -25,26 +25,26 @@ const reciveGift = () => {
 }
 
 const untilTime = () => {
-  let currentTime = new Date();
-  let hour = currentTime.getHours();
-  let minute = currentTime.getMinutes();
-  let second = currentTime.getSeconds();
-  let milliseconds = currentTime.getMilliseconds();
-  let isRight = hour === testTime[0] && minute === testTime[1] && second === testTime[2] && ( milliseconds >= testTime[3][0] && milliseconds <= testTime[3][1] );
-  if(!isRight) {
-    sleep(80);
-    return untilTime();
-  } else {
-    refresh();
-    reciveGift();
-    return true;
+  let isRight = false;
+  while (!isRight) {
+    let currentTime = new Date();
+    let hour = currentTime.getHours();
+    let minute = currentTime.getMinutes();
+    let second = currentTime.getSeconds();
+    let milliseconds = currentTime.getMilliseconds();
+    isRight = hour === testTime[0] && minute === testTime[1] && second === testTime[2] && ( milliseconds >= testTime[3][0] && milliseconds <= testTime[3][1] );
+    log(isRight);
+    sleep(40);
   }
+  refresh();
+  reciveGift();
 }
 
 const start = () => {
   auto(); // 自动开启无障碍服务
   events.observeKey(); // 启用按键监听
   events.setKeyInterceptionEnabled(['volume_down'], true); // 屏蔽音量键
+  toastLog('程序启动成功，请按下音量下键启动');
   events.onKeyDown('volume_down', function() {
     run();
   })
